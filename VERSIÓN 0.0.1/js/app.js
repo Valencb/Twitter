c911406ce84c7a200d81d4581612aa5d7a2fb1c1
Valencb <valenciablancalu@gmail.com>
2018-05-17 23:49:42 -0500

    updated version 0.0.2

--- VERSIÓN 0.0.1/js/app.js
+++ VERSIÓN 0.0.1/js/app.js
@@ -1,5 +1,5 @@
 var note=document.createTextNode("Que estas pensando....");
-var tittle=document.createElement('h2');
+var tittle=document.createElement('h3');
 tittle.id="thinking"
 var text= document.createElement('input');
 text.id= "textThink";
@@ -8,15 +8,16 @@ var tweet= document.createElement('button');
 tweet.id="bnt-tweet";
 tweet.innerHTML= "Tweet";
 var blockTweet=document.createElement('div');
+blockTweet.id="text"
 var tweetNote=document.getElementById('element');
 tittle.appendChild(note);
 blockTweet.appendChild(tittle);
 tweetNote.appendChild(blockTweet)
-form.appendChild(text);
 form.appendChild(tweet);
 blockTweet.appendChild(form);
 tweetNote.appendChild(blockTweet);
 blockTweet.appendChild(tweet);
+form.appendChild(text);
 
 var clickTweeter= document.getElementById("bnt-tweet");
 clickTweeter.addEventListener('click', function(){
@@ -26,13 +27,21 @@ clickTweeter.addEventListener('click', function(){
       alert('Debes ingresar un mensaje');
       return false;
     }
-var noteValue = document.createTextNode(valueTweet);
-console.log(noteValue);
+
 var blockNote = document.createElement('p');
 var block = document.createElement('div');
 var blockOnHtml = document.getElementById('twetear');
-blockNote.appendChild(noteValue);
+var blockIn = document.getElementById('block-center');
+var textNote1= document.getElementById('element')
+
 block.appendChild(blockNote);
+blockIn.appendChild(textNote1);
 blockOnHtml.appendChild(block);
+blockIn.appendChild(blockOnHtml);
+var noteValue = document.createTextNode(valueTweet);
+console.log(noteValue);
+blockNote.appendChild(noteValue);
+
+
 
 });
